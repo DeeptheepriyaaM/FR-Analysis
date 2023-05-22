af0ab1a8d5931dd31295291ce06ca65f6f5d0a04
Madhu <65707202+madhuammulu8@users.noreply.github.com>
2023-05-21 17:30:32 -0700

    Delete Timesheet.py

--- pages/Timesheet.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-option = st.selectbox(
-    'Your Name',
-    ('Abdul Kalam Azad', ' E.Subashini', 'Madhusudhan T','P. Mythili','S.Goutham','J.Richard Franklin','VA Naveen','A Sundhareshwaran','M Deepthee Priyaa'))
-
-st.write('You selected:', option)
