25e7423540f66d11abd721e15c7dfa7ec61d11a0
Madhu <65707202+madhuammulu8@users.noreply.github.com>
2023-05-21 21:24:09 -0700

    Adding Timesheet code to the Repo

---
+++ pages/_⏳_Timesheet.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+option = st.selectbox(
+    'Your Name',
+    ('Abdul Kalam Azad', ' E.Subashini', 'Madhusudhan T','P. Mythili','S.Goutham','J.Richard Franklin','VA Naveen','A Sundhareshwaran','M Deepthee Priyaa'))
+
