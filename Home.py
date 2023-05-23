6bffdc9d538f553703059c160109f1e857da7f04
Madhu <65707202+madhuammulu8@users.noreply.github.com>
2023-05-22 23:54:01 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.subheader("Welcome to the FR Team page where we can analyse our previous data")
+
+st.write(":blue[Timesheets]: to Fill your tasks you have worked on for FR /Hope Trust Activites")
+
+st.write(":blue[HopeTrust Expense]: You can submit you receipts here ")
+
+st.write(":blue[Monthly Funds Tracker]: We can visualize our Monthly funds data,Track the Growth rate and analyze for future use")
+
+st.write(":blue[Event Contributors]: We can visualize our Event Contributons done by our volunter, we can filter based on amount of donations and display it in chart")
